e91f3f292b451f2e42b9ba4980a5a4dde287237f
shatha893 <49248849+shatha893@users.noreply.github.com>
2021-03-19 14:57:54 +0200

    sorted out the code a bit

--- src/containers/RegistrationPage/RegistrationPage.js
+++ src/containers/RegistrationPage/RegistrationPage.js
@@ -7,76 +7,165 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import classes from './RegistrationPage.module.css';
 import Button from 'react-bootstrap/Button';
-// import Axios from 'axios';
 
 class RegistrationPage extends Component{
 
+    //The state of my class component which consists of the inputs for the page's form
     state = {
-        firstName:"",
-        lastName:"",
-        email:"",
-        password:"",
-        profilePic:""
+        firstName:{
+            value:"",
+            label:"First Name",
+            type:"text",
+            controlId:"formBasicFirstName"
+        },
+        lastName:{
+            value:"",
+            label:"Last Name",
+            type:"text",
+            controlId:"formBasicLastName"
+        },
+        email:{
+            value:"",
+            label:"Email",
+            type:"email",
+            controlId:"formBasicEmail"
+        },
+        password:{
+            value:"",
+            label:"Password",
+            type:"password",
+            controlId:"formBasicPassword"
+        },
+        phoneNum:{
+            value:"",
+            label:"Phone Number",
+            type:"text",
+            controlId:"formBasicPhoneNum"
+        }
     }
+
+    //Function to handle the change in the input "Form Control" value
+    handleChange = (controlId,event)=>{
+        switch(controlId)
+        {
+            case "formBasicFirstName":
+                this.setState({firstName:{...this.state.firstName,value:event.target.value}});
+                console.log(this.state.firstName.value);
+                break;
+            case "formBasicLastName":
+                this.setState({lastName:{...this.state.lastName,value:event.target.value}});
+                break;
+            case "formBasicEmail":
+                this.setState({email:{...this.state.email,value:event.target.value}});
+                break;
+            case "formBasicPassword":
+                this.setState({password:{...this.state.password,value:event.target.value}});
+                break;
+            default:
+                this.setState({phoneNum:{...this.state.phoneNum,value:event.target.value}});
+                break;
+        }
+    }
+
+
     render(){
+        
         return(
+            // Used Bootstraps container for it's easy positioning and styling 
             <Container fluid={+true} className={classes.EverythingContainer}>
+
+                {/*_______The header that will be viewed in all pages_________*/}
                 <Row>
                     <Header hideMenu = {false}/>
                 </Row>
+
+                {/*________This is the Row where the registration page content exists________*/}
                 <Row className={classes.RegistrationContainer}>
-                    <Col >
+                    <Col>
+                        {/* ___The login part___ */}
                         <div className={classes.LoginBox}>
-                            Already have an account?
-                             {/* Login Button */}
-                             <Button className={classes.LoginButton} size="lg">
-                                Login
-                             </Button>
+                             Already have an account?
+                             <Button 
+                             className={classes.LoginButton} 
+                             size="lg"> Login </Button>
                         </div>
                     </Col>
                     <Col>
-                        <div className={classes.RegistrationBox}>
-                            <Row>
-                                <Col >
-                                    <Form.Group controlId="formBasicFirstName" className={classes.Titles}>
-                                        <Form.Label>First Name </Form.Label>
-                                        <Form.Control type="text" className={classes.inputs}/>
-                                    </Form.Group>
-                                    
-                                    <Form.Group controlId="formBasicLastName" className={classes.Titles}>
-                                        <Form.Label>Last Name</Form.Label>
-                                        <Form.Control type="text" className={classes.inputs}/>
+                        <Form>
+                            <Form.Row className={classes.RegistrationBox}> 
+
+                            {/*___First Column of form___*/}
+                            <Col>
+                                {/*First name input*/}
+                                <Form.Group 
+                                controlId={this.state.firstName.controlId} 
+                                className={classes.Titles}>
+                                    <Form.Label> {this.state.firstName.label} </Form.Label>
+                                    <Form.Control 
+                                    type={this.state.firstName.type} 
+                                    onChange={(event)=>this.handleChange(this.state.firstName.controlId,event)}
+                                    className={classes.inputs}/>
+                                </Form.Group>
+
+                                    {/* Last name input */}
+                                    <Form.Group 
+                                    controlId={this.state.lastName.controlId} 
+                                    className={classes.Titles}>
+                                        <Form.Label> {this.state.lastName.label} </Form.Label>
+                                        <Form.Control 
+                                        type={this.state.lastName.type} 
+                                        onChange={(event)=>this.handleChange(this.state.lastName.controlId,event)}
+                                        className={classes.inputs}/>
                                     </Form.Group>
 
-                                    <Form.Group controlId="formBasicEmail" className={classes.Titles}>
-                                        <Form.Label> Email </Form.Label>
-                                        <Form.Control type="email" className={classes.inputs}/>
+                                    {/*Email input*/}
+                                    <Form.Group 
+                                    controlId={this.state.email.controlId} 
+                                    className={classes.Titles}>
+                                        <Form.Label> {this.state.email.label} </Form.Label>
+                                        <Form.Control 
+                                        type={this.state.email.type} 
+                                        onChange={(event)=>this.handleChange(this.state.email.controlId,event)}
+                                        className={classes.inputs}/>
                                     </Form.Group>
                                 </Col>
 
+                                {/* The second column of the form */}
                                 <Col>
-                                    {/* Password Input */}
-                                    <Form.Group controlId="formBasicPassword" className={classes.Titles}>
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" className={classes.inputs}/>
-                                    </Form.Group>
-
-                                    {/* Profile picture */}
-                                    <Form>
-                                        <Form.Group>
-                                            <Form.File  label="Profile Picture" className={classes.ProfileAvatar}/>
+                                    {/*Password form input*/}
+                                    <Form.Group 
+                                        controlId={this.state.password.controlId} 
+                                        className={classes.Titles}>
+                                            <Form.Label> {this.state.password.label} </Form.Label>
+                                            <Form.Control 
+                                            type={this.state.password.type} 
+                                            onChange={(event)=>this.handleChange(this.state.password.controlId,event)}
+                                            className={classes.inputs}/>
                                         </Form.Group>
-                                    </Form>
 
-                                    {/* Registration Button */}
-                                    <Button className={classes.SubmitButton}  onClick size="lg">
-                                        Submit
-                                    </Button>
+                                        {/*Phone Number form input*/}
+                                        <Form.Group 
+                                        controlId={this.state.phoneNum.controlId} 
+                                        className={classes.Titles}>
+                                            <Form.Label> {this.state.phoneNum.label} </Form.Label>
+                                            <Form.Control 
+                                            type={this.state.phoneNum.type} 
+                                            onChange={(event)=>this.handleChange(this.state.phoneNum.controlId,event)}
+                                            className={classes.inputs}/>
+                                        </Form.Group>
+
+                                        {/* Registration Button */}
+                                        <Button 
+                                        className={classes.SubmitButton}  
+                                        // onClick 
+                                        size="lg">  Submit  </Button>
                                 </Col>   
-                            </Row>
-                        </div>
+                            </Form.Row>
+                        </Form>
                     </Col>
                 </Row>
+
+                {/*_______The Footer that will also appear in all pages______*/}
                 <Row>
                     <Footer></Footer>
                 </Row>
